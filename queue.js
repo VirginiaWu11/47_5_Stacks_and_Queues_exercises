d75d1253062a124a466dbb5fa95522473271a64b
Virginia Wu <virginiawu11@gmail.com>
2021-12-25 23:08:47 -0500

    complete queue

--- queue.js
+++ queue.js
@@ -19,20 +19,43 @@ class Queue {
 
   /** enqueue(val): add new value to end of the queue. Returns undefined. */
 
-  enqueue(val) {}
+  enqueue(val) {
+    const newNode = new Node(val);
+
+    if (!this.first) {
+      this.first = newNode;
+      this.last = newNode;
+    } else {
+      this.last.next = newNode;
+      this.last = newNode;
+    }
+    this.size++;
+  }
 
   /** dequeue(): remove the node from the start of the queue
    * and return its value. Should throw an error if the queue is empty. */
 
-  dequeue() {}
+  dequeue() {
+    if (this.size === 0) {
+      throw new Error("Queue is empty");
+    }
+    const firstVal = this.first.val;
+    this.first = this.first.next;
+    this.size--;
+    return firstVal;
+  }
 
   /** peek(): return the value of the first node in the queue. */
 
-  peek() {}
+  peek() {
+    return this.first.val;
+  }
 
   /** isEmpty(): return true if the queue is empty, otherwise false */
 
-  isEmpty() {}
+  isEmpty() {
+    return this.size === 0 ? true : false;
+  }
 }
 
 module.exports = Queue;
